9668ca57a6d83b8a49283c50c7d0eb22e1ff8b92
HMPhuoc <huynhminhphuoc01@gmail.com>
2022-09-18 09:51:58 +0700

    database
    Tạo cơ sở dữ liệu
    Thêm một vài dữ liệu
    Diagram

---
+++ database/FASTFOOD.sql
@@ -0,0 +1,207 @@
+﻿create database FASTFOOD
+go
+
+use FASTFOOD
+go
+
+create table EMPLOYEE(
+	employee_id char(10) primary key,
+	employee_name nvarchar(100),
+	phone_number char(10) null,
+	gender bit not null,
+	position nvarchar(20)	
+)
+go
+
+create table ACCOUNT(
+	employee_id char(10) references EMPLOYEE(employee_id),
+	passwd char(10) not null,
+	permission bit,
+	primary key(employee_id)
+)
+go
+
+
+
+create table PRODUCT(
+	product_id int identity(1,1) primary key,
+	product_name nvarchar(50) not null,
+	price float constraint check_price check(price>=0) not null,
+	update_date datetime not null,
+	detail nvarchar(50) null
+)
+go
+
+create table VOUCHER(
+	voucher_id char(10) primary key,
+	apply_price float constraint min_money check (apply_price>=0) not null,
+	percent_off int constraint sales_off check (percent_off between 0 and 50),
+	max_money float constraint max_sales check(max_money<=100000),
+	used bit,
+)
+go
+
+create table BILL(
+	bill_id char(10) primary key,
+	date_created datetime not null,
+	employee_id char(10) references EMPLOYEE(employee_id)not null,
+	voucher_id char(10) references VOUCHER(voucher_id) null,
+	bill_price float constraint check_bill_price check(bill_price>=0),
+	final float constraint check_final_money check (final>=0),
+)
+go
+
+create table BILL_DETAIL(
+	bill_id char(10) references BILL(bill_id),
+	product_id int references PRODUCT(product_id),
+	price float,
+	amount int constraint check_amount_detail check(amount>0),
+	primary key(bill_id,product_id)
+)
+go
+
+
+--Add
+create proc add_Employee
+	@id char(10),
+	@name nvarchar(100),
+	@phone char(10),
+	@sex bit,
+	@position nvarchar(20)
+as
+	insert into EMPLOYEE(employee_id,employee_name,phone_number,gender,position)
+	values (@id,@name,@phone,@sex,@position)
+go
+
+create proc add_Product
+	@name nvarchar(50),
+	@price float,
+	@update_date datetime,
+	@detail nvarchar(50)
+as 
+	insert into PRODUCT(product_name,price,update_date,detail)
+	values (@name,@price,@update_date,@detail)
+go
+
+create proc add_Voucher
+	@id char(10),
+	@apply_price float,
+	@percent int,
+	@max_money float
+as
+	insert into VOUCHER(voucher_id,apply_price,percent_off,max_money,used)
+	values (@id,@apply_price,@percent,@max_money, 0)
+go
+
+create proc add_Bill
+	@bill_id char(10),
+	@date datetime,
+	@employee_id char(10),
+	@voucher_id char(10)
+as
+	insert into BILL(bill_id,date_created,employee_id,voucher_id,bill_price,final)
+	values (@bill_id,@date,@employee_id,@voucher_id,0,0)
+go
+
+create proc add_Bill_detail
+	@bill_id char(10),
+	@product_id int,
+	@amount int
+as
+	insert into BILL_DETAIL(bill_id,product_id,amount)
+	values(@bill_id,@product_id,@amount)
+go
+
+create proc add_Account
+	@id char(10),
+	@pass char(10),
+	@permit bit
+as
+	insert into ACCOUNT(employee_id,passwd,permission)
+	values (@id,@pass,@permit)
+go
+
+--Update
+create proc update_Bill
+	@id char(10)
+as 
+	update BILL
+	set bill_price=(select sum(price*amount) from BILL_DETAIL bd where bd.bill_id=@id),final=bill_price
+	where bill_id=@id
+go
+
+
+create proc update_Bill_Detail
+	@id char(10)
+as
+	update BILL_DETAIL
+	set price = (select price from PRODUCT where PRODUCT.product_id=BILL_DETAIL.product_id)
+	where bill_id=@id
+go
+
+
+
+create proc update_Product
+	@id int,
+	@name nvarchar(50),
+	@price float,
+	@date datetime,
+	@detail nvarchar(50)
+as
+	update PRODUCT
+	set product_name=@name, price=@price,update_date=@date,detail=@detail
+	where product_id=@id
+go
+
+
+create proc update_Voucher_Used
+	@id char(10)
+as
+	update VOUCHER
+	set used=1
+	where voucher_id=@id
+go
+
+create proc update_Employee
+	@id char(10),
+	@name nvarchar(100),
+	@phone char(10),
+	@position nvarchar(20)
+as
+	update EMPLOYEE
+	set employee_name=@name, phone_number=@phone, position=@position
+	where employee_id=@id
+go
+
+
+
+
+--Delete
+
+
+--get data
+
+--Doanh thu theo ngay
+create function Revenue_Day
+(@date datetime)
+returns float
+as
+begin 
+	declare @revenue float
+	set @revenue=(select sum(final) from BILL b where DAY(b.date_created)=DAY(@date) and MONTH(b.date_created)=MONTH(@date) and YEAR(b.date_created)=YEAR(@date))
+	return @revenue
+end
+go
+
+--Doanh thu theo thang
+create function Revenue_Month
+(@date datetime)
+returns float
+as
+begin
+	declare @revenue float
+	set @revenue=(select sum(final) from BILL b where MONTH(b.date_created)=MONTH(@date) and YEAR(b.date_created)=YEAR(@date))
+	return @revenue
+end
+go
+
---
+++ database/insert_FASTFOOD.sql
@@ -0,0 +1,48 @@
+﻿use FASTFOOD
+go
+
+
+exec add_Employee 'abc',N'Phước', 0357648144,false,'administrator'
+exec add_Employee 'aaa',N'Nhật', 0123456789,false,'manager'
+exec add_Employee 'bbb',N'Đồng', 0123456789,false,'manager'
+exec add_Employee 'ccc',N'Quang',0123456789,false,'manager'
+
+exec add_Account 'abc',0357648144,true
+exec add_Account 'aaa',0123456789,false
+exec add_Account 'bbb',0123456789,true
+exec add_Account 'ccc',0123456789,false
+
+exec add_Product N'Cà phê',20000,'2000-2-2', N'Không có'
+exec add_Product N'Gà chiên',50000,'2000-2-2',N'Không có'
+exec add_Product N'CocaCola',10000,'2000-2-2',N'Không có'
+exec add_Product N'Cơm trưa',30000,'2022-9-17','Không có'
+exec add_Product N'Bún', 30000,'2022-9-17','Không có'
+exec add_Product N'Bánh mì',15000,'2022-9-17',N'Không có'
+
+exec add_Voucher 'Khong',0,0,0
+exec add_Voucher 'giamgiane',20000,20,50000
+
+exec add_Bill 'bill1','9-17-2022','aaa','Khong'
+exec add_Bill 'bill2','9-17-2022','aaa','Khong'
+exec add_Bill 'bill3','2022-9-17','bbb','Khong'
+exec add_Bill 'bill4','2022-9-17','ccc','Khong'
+
+
+exec add_Bill_detail 'bill1',1,2
+exec add_Bill_detail 'bill1',2,2
+exec add_Bill_detail 'bill2',2,1
+exec add_Bill_detail 'bill3',5,2
+exec add_Bill_detail 'bill4',6,1
+
+
+
+exec update_Bill_Detail 'bill1'
+exec update_Bill_Detail 'bill2'
+exec update_Bill_Detail 'bill3'
+exec update_Bill_Detail 'bill4'
+
+exec update_Bill 'bill1'
+exec update_Bill 'bill2'
+exec update_Bill 'bill3'
+exec update_Bill 'bill4'
+
