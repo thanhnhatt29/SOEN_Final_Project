1ebe7ef8da5952c2d7d86db7d36e55f9fc5f48af
ThachQuang <20133083@student.hcmute.edu.vn>
2022-10-02 19:27:26 +0700

    Code DAL, BLL cho bill

---
+++ UI/BLL/BillBLL.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using DAL;
+namespace BLL
+{
+    public class BillBLL
+    {
+        BillDAL bill_DAL = new BillDAL();
+        public List<BILL> getDataBillDLL()
+        {
+            return bill_DAL.getDataBillDAL();
+        }
+
+        public string addBillBLL(string bill_id, DateTime date_created, string employee_id, string voucher_id, int bill_price, int off_money)
+        {
+            if (bill_DAL.addBillDAL(bill_id, date_created, employee_id, voucher_id, bill_price, off_money))
+            {
+                return "Thành công";
+            }
+            else
+                return "Thất bại";
+        }
+
+        public string updateBillBLL(string bill_id, DateTime date_created, string employee_id, string voucher_id, int bill_price, int off_money)
+        {
+            if (bill_DAL.updateBillDAL(bill_id, date_created, employee_id, voucher_id, bill_price, off_money))
+            {
+                return "Thành công";
+            }
+            else
+                return "Thất bại";
+        }
+    }
+}
---
+++ UI/DAL/BillDAL.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class BillDAL
+    {
+        public List<BILL> getDataBillDAL()
+        {
+            List<BILL> data = new List<BILL>();
+            using(FASTFOODEntities db = new FASTFOODEntities())
+            {
+                var table = from c in db.BILLs
+                          select c;
+                foreach(var bill in table)
+                {
+                    data.Add(bill);
+                }
+                return data;
+            }
+        }
+
+        public bool updateBillDAL(string bill_id, DateTime date_created, string employee_id, string voucher_id, int bill_price, int off_money)
+        {
+            using (FASTFOODEntities db = new FASTFOODEntities())
+            {
+                var table = from c in db.BILLs
+                            select c;
+                foreach (var t in table)
+                {
+                    if (t.bill_id == bill_id)
+                    {
+                        t.date_created = date_created;
+                        t.employee_id = employee_id;
+                        t.voucher_id = voucher_id;
+                        t.bill_price = bill_price;
+                        t.off_money = off_money;
+                    }
+                }
+                db.SaveChanges();
+                return true;
+            }
+        }
+
+        public bool addBillDAL(string bill_id, DateTime date_created, string employee_id, string voucher_id, int bill_price, int off_money)
+        {
+            using (FASTFOODEntities db = new FASTFOODEntities())
+            {
+                BILL bill = new BILL();
+
+                bill.bill_id = bill_id;
+                bill.date_created = date_created;
+                bill.employee_id = employee_id;
+                bill.voucher_id = voucher_id;
+                bill.bill_price = bill_price;
+                bill.off_money = off_money;
+
+                db.BILLs.Add(bill);
+                db.SaveChanges();
+                return true;
+            }
+        }
+    }
+}
