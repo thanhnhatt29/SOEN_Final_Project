01309bce042d3b247ea11b8863e8f475fcc4b727
FlamesThanhDong <92565972+FlamesThanhDong@users.noreply.github.com>
2022-10-07 13:49:56 +0700

    Voucher update

--- UI/UI/Admin/VoucherManage.Designer.cs
+++ UI/UI/Admin/VoucherManage.Designer.cs
@@ -28,65 +28,231 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.money = new System.Windows.Forms.TextBox();
+            this.percent = new System.Windows.Forms.TextBox();
+            this.price = new System.Windows.Forms.TextBox();
+            this.voucher = new System.Windows.Forms.TextBox();
+            this.max_money = new System.Windows.Forms.Label();
+            this.lb = new System.Windows.Forms.Label();
+            this.apply_price = new System.Windows.Forms.Label();
+            this.voucher_id = new System.Windows.Forms.Label();
+            this.Information = new System.Windows.Forms.GroupBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Information.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "True",
+            "False"});
+            this.comboBox1.Location = new System.Drawing.Point(260, 156);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(135, 24);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(211, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "used:";
+            // 
+            // money
+            // 
+            this.money.Location = new System.Drawing.Point(443, 117);
+            this.money.Name = "money";
+            this.money.Size = new System.Drawing.Size(133, 22);
+            this.money.TabIndex = 7;
+            // 
+            // percent
+            // 
+            this.percent.Location = new System.Drawing.Point(443, 38);
+            this.percent.Name = "percent";
+            this.percent.Size = new System.Drawing.Size(133, 22);
+            this.percent.TabIndex = 6;
+            // 
+            // price
+            // 
+            this.price.Location = new System.Drawing.Point(140, 112);
+            this.price.Name = "price";
+            this.price.Size = new System.Drawing.Size(130, 22);
+            this.price.TabIndex = 5;
+            // 
+            // voucher
+            // 
+            this.voucher.Location = new System.Drawing.Point(140, 43);
+            this.voucher.Name = "voucher";
+            this.voucher.Size = new System.Drawing.Size(130, 22);
+            this.voucher.TabIndex = 4;
+            // 
+            // max_money
+            // 
+            this.max_money.AutoSize = true;
+            this.max_money.Location = new System.Drawing.Point(318, 117);
+            this.max_money.Name = "max_money";
+            this.max_money.Size = new System.Drawing.Size(87, 17);
+            this.max_money.TabIndex = 3;
+            this.max_money.Text = "max_money:";
+            // 
+            // lb
+            // 
+            this.lb.AutoSize = true;
+            this.lb.Location = new System.Drawing.Point(321, 38);
+            this.lb.Name = "lb";
+            this.lb.Size = new System.Drawing.Size(84, 17);
+            this.lb.TabIndex = 2;
+            this.lb.Text = "percent_off:";
+            // 
+            // apply_price
+            // 
+            this.apply_price.AutoSize = true;
+            this.apply_price.Location = new System.Drawing.Point(21, 117);
+            this.apply_price.Name = "apply_price";
+            this.apply_price.Size = new System.Drawing.Size(85, 17);
+            this.apply_price.TabIndex = 1;
+            this.apply_price.Text = "apply_price:";
+            // 
+            // voucher_id
+            // 
+            this.voucher_id.AutoSize = true;
+            this.voucher_id.Location = new System.Drawing.Point(24, 43);
+            this.voucher_id.Name = "voucher_id";
+            this.voucher_id.Size = new System.Drawing.Size(82, 17);
+            this.voucher_id.TabIndex = 0;
+            this.voucher_id.Text = "voucher_id:";
+            // 
+            // Information
+            // 
+            this.Information.Controls.Add(this.comboBox1);
+            this.Information.Controls.Add(this.label2);
+            this.Information.Controls.Add(this.money);
+            this.Information.Controls.Add(this.percent);
+            this.Information.Controls.Add(this.price);
+            this.Information.Controls.Add(this.voucher);
+            this.Information.Controls.Add(this.max_money);
+            this.Information.Controls.Add(this.lb);
+            this.Information.Controls.Add(this.apply_price);
+            this.Information.Controls.Add(this.voucher_id);
+            this.Information.Location = new System.Drawing.Point(12, 351);
+            this.Information.Name = "Information";
+            this.Information.Size = new System.Drawing.Size(679, 194);
+            this.Information.TabIndex = 18;
+            this.Information.TabStop = false;
+            this.Information.Text = "Information";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(246, 10);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(81, 25);
+            this.button6.TabIndex = 17;
+            this.button6.Text = "search";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(99, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(129, 22);
+            this.textBox1.TabIndex = 16;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(714, 516);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(96, 37);
+            this.button5.TabIndex = 15;
+            this.button5.Text = "exit";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(714, 419);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(96, 37);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "update";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(714, 468);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(96, 37);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(714, 369);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 37);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 18);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Voucher:";
             // 
             // dataGridView1
             // 
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 133);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 52);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(776, 305);
-            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.Size = new System.Drawing.Size(798, 282);
+            this.dataGridView1.TabIndex = 10;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(41, 49);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(61, 17);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Voucher";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(683, 34);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(87, 46);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Create voucher";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(683, 86);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(87, 41);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "show";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // VoucherManage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(824, 563);
+            this.Controls.Add(this.Information);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "VoucherManage";
             this.Text = "VourcherManage";
+            this.Load += new System.EventHandler(this.VoucherManage_Load);
+            this.Information.ResumeLayout(false);
+            this.Information.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -95,9 +261,24 @@
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox money;
+        private System.Windows.Forms.TextBox percent;
+        private System.Windows.Forms.TextBox price;
+        private System.Windows.Forms.TextBox voucher;
+        private System.Windows.Forms.Label max_money;
+        private System.Windows.Forms.Label lb;
+        private System.Windows.Forms.Label apply_price;
+        private System.Windows.Forms.Label voucher_id;
+        private System.Windows.Forms.GroupBox Information;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- UI/UI/Admin/VoucherManage.cs
+++ UI/UI/Admin/VoucherManage.cs
@@ -14,34 +14,109 @@ namespace UI
 {
     public partial class VoucherManage : Form
     {
-        SqlConnection con = new SqlConnection(@"Data Source=LAPTOP-0460STTB;Initial Catalog=FASTFOOD;Integrated Security=True");
+        SqlConnection connection;
+        SqlCommand command;
+        string str = "Data Source=LAPTOP-0460STTB;Initial Catalog=FASTFOOD;Integrated Security=True";
+        SqlDataAdapter da = new SqlDataAdapter();
+        DataTable dt = new DataTable();
 
         public VoucherManage()
         {
             InitializeComponent();
         }
+        public void LoadData()
+        {
+            command = connection.CreateCommand();
+            command.CommandText = "select * from VOUCHER";
+            da.SelectCommand = command;
+            dt.Clear();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            command = connection.CreateCommand();
+            command.CommandText = "insert into VOUCHER values('" + voucher.Text + "','" + price.Text + "','" + percent.Text + "','" + money.Text + "','" + comboBox1.Text + "')";
+            command.ExecuteNonQuery();
+            LoadData();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                command = connection.CreateCommand();
+                command.CommandText = "select * from VOUCHER where voucher_id like('%" + textBox1.Text + "%')";
+                command.ExecuteNonQuery();
+                DataTable dt = new DataTable();
+                SqlDataAdapter a = new SqlDataAdapter(command);
+                a.Fill(dt);
+                int i = Convert.ToInt32(dt.Rows.Count.ToString());
+                dataGridView1.DataSource = dt;
+                if (i == 0)
+                {
+                    MessageBox.Show("No Found voucher!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                command = connection.CreateCommand();
+                command.CommandText = "update VOUCHER set apply_price = " + price.Text + " , off_percent = " + percent.Text + ", max_money = " + money.Text + " ,used='" + comboBox1.Text + "' where voucher_id='" + voucher.Text + "'";
+                command.ExecuteNonQuery();
+                LoadData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                command = connection.CreateCommand();
+                command.CommandText = "delete from VOUCHER where voucher_id='" + voucher.Text + "'";
+                command.ExecuteNonQuery();
+                LoadData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-
+            // voucher.ReadOnly = true;
+            int i;
+            i = dataGridView1.CurrentRow.Index;
+            voucher.Text = dataGridView1.Rows[i].Cells[0].Value.ToString();
+            price.Text = dataGridView1.Rows[i].Cells[1].Value.ToString();
+            percent.Text = dataGridView1.Rows[i].Cells[2].Value.ToString();
+            money.Text = dataGridView1.Rows[i].Cells[3].Value.ToString();
+            comboBox1.Text = dataGridView1.Rows[i].Cells[4].Value.ToString();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void VoucherManage_Load(object sender, EventArgs e)
         {
-
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            con.Open();
-            SqlCommand cmd = con.CreateCommand();
-            cmd.CommandType = CommandType.Text;
-            cmd.CommandText = "select * from VOUCHER";
-            DataTable dt = new DataTable();
-            SqlDataAdapter da = new SqlDataAdapter(cmd);
-            da.Fill(dt);
-            dataGridView1.DataSource = dt;
-            con.Close();
+            connection = new SqlConnection(str);
+            connection.Open();
+            LoadData();
         }
     }
 }
